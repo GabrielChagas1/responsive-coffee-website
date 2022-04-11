1ec212bc5c624330b7c7f3af850f9b130b87ddc1
Gabriel Chagas <gabrielserq@outlook.com>
2022-04-10 22:05:53 -0300

    chore: creating js to show scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,6 +64,12 @@ function activeProducts(){
 linkProducts.forEach(i => i.addEventListener('click', activeProducts))
 
 /*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
